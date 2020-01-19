a27a6e4797875a8c48b08aa7cb3c95869d804712
lewis orenge <lewisorenge@gmail.com>
2020-01-19 23:22:10 +0300

    Update scripts.js
    skill add

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ function typeOnTerminal() {
 
     var waitAndBreak = '^700 <br/>';
     var start = '<span class="margin-left--10 color-blue font-size-14 !important">➜:~</span>';
-    var myStack = start + whiten('Skill Set?') + waitAndBreak + ['Kotlin', 'Java', 'MySQL', 'Laravel', 'Vue.js', 'Adobe Creative Suite','Docker', 'AWS','Learning Python for Data.'].join(',^500 ');
+    var myStack = start + whiten('Skill Set?') + waitAndBreak + ['Kotlin', 'Java', 'MySQL', 'Vue.js/Javascript', 'Laravel/PHP', 'Adobe Creative Suite', 'REST API', 'Docker', 'AWS', 'Learning Python for Data.'].join(',^500 ');
     var experience = getYearsBetween(new Date("February 1 2016 00:00"));
     var yearsOfExperience = waitAndBreak + start + whiten('Experience?') + waitAndBreak + experience;
     var funGames = ['Travelling,' + ' Music and ' + 'Dancing'].join('^500, ');
